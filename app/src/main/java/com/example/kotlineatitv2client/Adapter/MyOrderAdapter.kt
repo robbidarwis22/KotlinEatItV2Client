be6781d84604057d51d701ec02ce52993ee8d83b
robbidarwis22 <robbidarwis22@gmail.com>
2020-08-03 21:40:57 +0700

    Show Order Detail

--- app/src/main/java/com/example/kotlineatitv2client/Adapter/MyOrderAdapter.kt
+++ app/src/main/java/com/example/kotlineatitv2client/Adapter/MyOrderAdapter.kt
@@ -1,14 +1,23 @@
 package com.example.kotlineatitv2client.Adapter
 
+import android.app.AlertDialog
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.kotlineatitv2client.Callback.IRecyclerItemClickListener
 import com.example.kotlineatitv2client.Common.Common
+import com.example.kotlineatitv2client.Database.CartItem
 import com.example.kotlineatitv2client.Model.OrderModel
 import com.example.kotlineatitv2client.R
 import java.lang.StringBuilder
@@ -27,13 +36,20 @@ private val orderList:MutableList<OrderModel>):
         simpleDateFormat = SimpleDateFormat("dd-MM-yyyy HH:mm:ss")
     }
 
-    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
-    {
+    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView),
+        View.OnClickListener {
         internal var img_order:ImageView?=null
         internal var txt_order_date:TextView?=null
         internal var txt_order_status:TextView?=null
         internal var txt_order_number:TextView?=null
         internal var txt_order_comment:TextView?=null
+
+        internal var iRecyclerItemClickListener: IRecyclerItemClickListener?=null
+
+        fun setListener(iRecyclerItemClickListener: IRecyclerItemClickListener)
+        {
+            this.iRecyclerItemClickListener = iRecyclerItemClickListener
+        }
 
         init {
             img_order = itemView.findViewById(R.id.img_order) as ImageView
@@ -41,6 +57,12 @@ private val orderList:MutableList<OrderModel>):
             txt_order_status = itemView.findViewById(R.id.txt_order_status) as TextView
             txt_order_number = itemView.findViewById(R.id.txt_order_number) as TextView
             txt_order_comment = itemView.findViewById(R.id.txt_order_comment) as TextView
+
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(p0: View?) {
+            iRecyclerItemClickListener!!.onItemClick(p0!!,adapterPosition)
         }
     }
 
@@ -75,7 +97,35 @@ private val orderList:MutableList<OrderModel>):
         holder.txt_order_comment!!.text = StringBuilder("Comment: ").append(orderList[position].comment)
         holder.txt_order_status!!.text = StringBuilder("Status: ").append(Common.convertStatusToText(orderList[position].orderStatus))
 
+        holder.setListener(object:IRecyclerItemClickListener{
+            override fun onItemClick(view: View, pos: Int) {
+                showDialog(orderList[pos].cartItemList)
+            }
+        })
     }
 
+    private fun showDialog(cartItemList: List<CartItem>?) {
+        val layout_dialog = LayoutInflater.from(context).inflate(R.layout.layout_dialog_order_detail,null)
+        val builder = AlertDialog.Builder(context)
+        builder.setView(layout_dialog)
+
+        val btn_ok = layout_dialog.findViewById<View>(R.id.btn_ok) as Button
+        val recycler_order_detail = layout_dialog.findViewById<View>(R.id.recycler_order_detail) as RecyclerView
+        recycler_order_detail.setHasFixedSize(true)
+        val layoutManger = LinearLayoutManager(context)
+        recycler_order_detail.layoutManager = layoutManger
+        recycler_order_detail.addItemDecoration(DividerItemDecoration(context,layoutManger.orientation))
+        val adapter = MyOrderDetailAdapter(context,cartItemList!!.toMutableList())
+        recycler_order_detail.adapter = adapter
+
+        //Show dialog
+        val dialog = builder.create()
+        dialog.show()
+        //custom dialog
+        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.window!!.setGravity(Gravity.CENTER)
+
+        btn_ok.setOnClickListener { dialog.dismiss() }
+    }
 
 }
